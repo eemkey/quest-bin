6a32aa13a7cce84a94c7a63ecbfa5332617c0efd
kathyfregoso <kathyfregoso@gmail.com>
2022-02-03 14:07:23 -0800

    Add more HTTP request headers

--- client/src/components/Request.js
+++ client/src/components/Request.js
@@ -1,26 +1,35 @@
 import React from "react";
 // accepts a request object as an argument
-// display headers and body 
+// display headers and body
 
-// will still change how we display the requests (add headers, etc), this is just to test it 
+// will still change how we display the requests (add headers, etc), this is just to test it
 
 const Request = ({ req }) => {
   return (
     <div>
       <div>
-          <p>Timestamp: {req.timestamp}</p>
-          <p>{req.method} {req.url}</p>
-          <p>Raw Body: {req.body}</p>
+        <p>Timestamp: {req.timestamp}</p>
+        <p>
+          {req.method} {req.url}
+        </p>
+        <p>Raw Body: {req.body}</p>
 
         <h3>Request Headers</h3>
-          <p>Host: {req.headers.host}</p>
-          <p>Accept: {req.headers.accept}</p>
-          <p>Cookie: {req.headers.cookie}</p> 
-          {/* <p>User-Agent: {req.headers.user-agent}</p> */}
+        {/* <p>Host: {req.headers.host}</p>
+        <p>Accept: {req.headers.accept}</p>
+        <p>Cookie: {req.headers.cookie}</p>
+        <p>User-Agent: {req.headers["user-agent"]}</p> */}
+        {Object.keys(req.headers).map((key) => {
+          return (
+            <p>
+              {key} : {req.headers[key]}
+            </p>
+          );
+        })}
         <hr></hr>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Request
+export default Request;
